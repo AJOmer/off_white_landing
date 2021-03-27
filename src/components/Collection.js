6f9df15f5d39d5c78cd1a1b973b893558e57290c
ahmed omer <ahmedjomer@gmail.com>
2021-03-26 17:56:27 -0700

    collections page

---
+++ src/components/Collection.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+    makeStyles, Grid, Button, useMediaQuery, useTheme
+} from '@material-ui/core';
+import Logo from '../assets/offwhite.png';
+
+// DESKTOP \\
+import offWhiteOne from '../assets/collection/desktop/offWhiteOne.jpg';
+import offWhiteTwo from '../assets/collection/desktop/offWhiteTwo.jpg'
+import offWhiteThree from '../assets/collection/desktop/offWhiteThree.jpg'
+import virgilIkeaOne from '../assets/collection/desktop/virgilIkeaOne.jpg'
+import virgilIkeaTwo from '../assets/collection/desktop/virgilIkeaTwo.jpg'
+import virgilIkeaThree from '../assets/collection/desktop/virgilIkeaThree.jpg'
+import virgilLouisOne from '../assets/collection/desktop/virgilLouisOne.jpg';
+import virgilLouisTwo from '../assets/collection/desktop/virgilLouisTwo.jpg';
+import virgilLouisThree from '../assets/collection/desktop/virgilLouisThree.jpg';
+import virgilNikeOne from '../assets/collection/desktop/virgilNikeOne.jpg';
+import virgilNikeTwo from '../assets/collection/desktop/virgilNikeTwo.jpg';
+import virgilNikeThree from '../assets/collection/desktop/virgilNikeThree.jpg';
+
+// MOBILE \\
+import offWhiteOneMobile from '../assets/collection/mobile/offWhiteOneMobile.jpg';
+import offWhiteTwoMobile from '../assets/collection/mobile/offWhiteTwoMobile.jpg'
+import offWhiteThreeMobile from '../assets/collection/mobile/offWhiteThreeMobile.jpg'
+import virgilIkeaOneMobile from '../assets/collection/mobile/virgilIkeaOneMobile.jpg'
+import virgilIkeaTwoMobile from '../assets/collection/mobile/virgilIkeaTwoMobile.jpg'
+import virgilIkeaThreeMobile from '../assets/collection/mobile/virgilIkeaThreeMobile.jpg'
+import virgilLouisOneMobile from '../assets/collection/mobile/virgilLouisOneMobile.jpg';
+import virgilLouisTwoMobile from '../assets/collection/mobile/virgilLouisTwoMobile.jpg';
+import virgilLouisThreeMobile from '../assets/collection/mobile/virgilLouisThreeMobile.jpg';
+import virgilNikeOneMobile from '../assets/collection/mobile/virgilNikeOneMobile.jpg';
+import virgilNikeTwoMobile from '../assets/collection/mobile/virgilNikeTwoMobile.jpg';
+import virgilNikeThreeMobile from '../assets/collection/mobile/virgilNikeThreeMobile.jpg';
+
+// TABLET \\
+import offWhiteOneTab from '../assets/collection/tablet/offWhiteOneTab.jpg';
+import offWhiteTwoTab from '../assets/collection/tablet/offWhiteTwoTab.jpg'
+import offWhiteThreeTab from '../assets/collection/tablet/offWhiteThreeTab.jpg'
+import virgilIkeaOneTab from '../assets/collection/tablet/virgilIkeaOneTab.jpg'
+import virgilIkeaTwoTab from '../assets/collection/tablet/virgilIkeaTwoTab.jpg'
+import virgilIkeaThreeTab from '../assets/collection/tablet/virgilIkeaThreeTab.jpg'
+import virgilLouisOneTab from '../assets/collection/tablet/virgilLouisOneTab.jpg';
+import virgilLouisTwoTab from '../assets/collection/tablet/virgilLouisTwoTab.jpg';
+import virgilLouisThreeTab from '../assets/collection/tablet/virgilLouisThreeTab.jpg';
+import virgilNikeOneTab from '../assets/collection/tablet/virgilNikeOneTab.jpg';
+import virgilNikeTwoTab from '../assets/collection/tablet/virgilNikeTwoTab.jpg';
+import virgilNikeThreeTab from '../assets/collection/tablet/virgilNikeThreeTab.jpg';
